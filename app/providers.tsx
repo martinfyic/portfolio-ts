ec46e2d0e8771ada16b883196c10c3bee655168a
Martin Ferreira <martin.f.yic@gmail.com>
2023-08-13 22:49:38 -0300

    feat: :sparkles: add app/providers.tsx to implement ThemeProvider at layout

---
+++ app/providers.tsx
@@ -0,0 +1,12 @@
+'use client';
+
+import { ReactNode } from 'react';
+import { ThemeProvider } from 'next-themes';
+
+export const Providers = ({ children }: { children: ReactNode }) => {
+  return (
+    <ThemeProvider attribute="class" enableSystem enableColorScheme>
+      {children}
+    </ThemeProvider>
+  );
+};
