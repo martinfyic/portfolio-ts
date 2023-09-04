46d41812962d21e14595eb97489ae67104e9bca8
Martin Ferreira <martin.f.yic@gmail.com>
2023-09-04 17:52:26 -0300

    feat: :recycle: implement HeroSection

--- app/page.tsx
+++ app/page.tsx
@@ -1,15 +1,16 @@
 import type { Metadata } from 'next';
+import { HeroSection } from '@/components';
 
 export const metadata: Metadata = {
-	title: 'Martin Ferreira | Full Stack Web Developer',
-	description:
-		'Portfolio de Martin Ferreira Yic, Desarrollador Web Full Stack, stack MERN, Next.js, CSS, HTML',
+  title: 'Martin Ferreira | Full Stack Web Developer',
+  description:
+    'Portfolio de Martin Ferreira Yic, Desarrollador Web Full Stack, stack MERN, Next.js, CSS, HTML',
 };
 
 export default function Home() {
-	return (
-		<>
-			<h1 className='text-center text-5xl'>Start Project 🚀</h1>
-		</>
-	);
+  return (
+    <main className="mx-auto max-w-3xl px-4 sm:px-6 md:max-w-5xl">
+      <HeroSection />
+    </main>
+  );
 }
