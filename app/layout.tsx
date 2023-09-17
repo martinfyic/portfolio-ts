38d529cfcf0744fefe7de52cae6d35ffe6570cb7
Martin Ferreira <martin.f.yic@gmail.com>
2023-09-17 13:02:02 -0300

    perf: :zap: implement animate.css at app/layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,7 @@
 import { ReactNode } from 'react';
 import { Navbar } from '@/components';
 import { Providers } from './providers';
+import 'animate.css';
 import '../styles/globals.css';
 
 import { Inter } from 'next/font/google';
@@ -8,17 +9,14 @@ import { Inter } from 'next/font/google';
 const inter = Inter({ subsets: ['latin'] });
 
 export default function RootLayout({ children }: { children: ReactNode }) {
-	return (
-		<html
-			lang='es'
-			suppressHydrationWarning
-		>
-			<body className={inter.className}>
-				<Providers>
-					<Navbar />
-					{children}
-				</Providers>
-			</body>
-		</html>
-	);
+  return (
+    <html lang="es" suppressHydrationWarning>
+      <body className={inter.className}>
+        <Providers>
+          <Navbar />
+          {children}
+        </Providers>
+      </body>
+    </html>
+  );
 }
