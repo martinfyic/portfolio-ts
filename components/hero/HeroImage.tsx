e1bb1643f47fc1c7c92b231577ab833efe34ef9f
Martin Ferreira <martin.f.yic@gmail.com>
2023-09-23 20:37:34 -0300

    style: :art: change import png profilPic to ProfilPic

--- components/hero/HeroImage.tsx
+++ components/hero/HeroImage.tsx
@@ -1,12 +1,12 @@
 import { FC } from 'react';
 import Image from 'next/image';
-import profilPic from '../../public/personal-foto.png';
+import ProfilPic from '../../public/personal-foto.png';
 
 export const HeroImage: FC = () => {
 	return (
 		<div className="md:w-1/2 md:mt-2 animate__animated animate__fadeIn animate__slow">
 			<Image
-				src={profilPic}
+				src={ProfilPic}
 				alt="Foto de Martin Ferreira"
 				priority
 				sizes="100vw"
