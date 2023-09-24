769631e0e9e495c07de8d4b94f2c19ca86f5d491
Martin Ferreira <martin.f.yic@gmail.com>
2023-09-24 17:12:58 -0300

    refactor: :lipstick: change some clases

--- components/about/AboutSection.tsx
+++ components/about/AboutSection.tsx
@@ -3,11 +3,11 @@ import { BiografiSection, SkillSection } from './';
 export const AboutSection = () => {
 	return (
 		<section id="about">
-			<div className="my-12 pb-12 md:pt-16 md:pb-48">
+			<div className="my-12 pb-12 md:pt-6 md:pb-48">
 				<h2
-					className="text-center font-bold text-6xl my-20"
+					className="text-center font-bold text-6xl mt-6 mb-12"
 					data-aos="fade-up"
-					data-aos-duration="2000"
+					data-aos-duration="800"
 				>
 					Sobre Mi
 					<hr className="w-6 h-1 mx-auto my-4 bg-primary border-0 rounded" />
