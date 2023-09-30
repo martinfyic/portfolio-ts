8741c4b102a7172062c12e3dea98c4049351158c
Martin Ferreira <martin.f.yic@gmail.com>
2023-09-30 18:18:42 -0300

    feat: :lipstick: add pages

---
+++ app/certificate/page.tsx
@@ -0,0 +1,4 @@
+const CertificatePage = () => {
+	return <h1>CertificatePage</h1>;
+};
+export default CertificatePage;
---
+++ app/project/page.tsx
@@ -0,0 +1,4 @@
+const ProjectPage = () => {
+	return <h1>ProjectPage</h1>;
+};
+export default ProjectPage;
