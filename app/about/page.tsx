63a3cc1f49bfb956096c2666c849c702a86ab838
Martin Ferreira <martin.f.yic@gmail.com>
2023-10-01 15:14:16 -0300

    refactor: :lipstick: page in construction

--- app/about/page.tsx
+++ app/about/page.tsx
@@ -1,4 +1,32 @@
+export const generateMetadata = async ({
+	params,
+}: {
+	params: { slug: string };
+}) => {
+	return {
+		title: 'Sobre Mi | Martin Ferreira',
+		authors: [
+			{
+				name: 'Martin Ferreira Yic',
+				url: 'https://www.linkedin.com/in/martin-ferreira-yic/',
+			},
+		],
+		icons: [
+			{
+				rel: 'icon',
+				url: 'data:image/svg+xml,<svg xmlns=%22http://www.w3.org/2000/svg%22 viewBox=%220 0 100 100%22><text y=%22.9em%22 font-size=%2290%22>👨🏻‍💻</text></svg>',
+			},
+		],
+	};
+};
+
 const AboutPage = () => {
-	return <h1>AboutPage</h1>;
+	return (
+		<section className="h-screen">
+			<h1 className="text-center text-4xl md:text-6xl mb-11">Sobre Mi</h1>
+			<p className="text-8xl text-center  mb-11">🏗️</p>
+			<p className="text-2xl text-center">👷🏻 En construcción ⚒️</p>
+		</section>
+	);
 };
 export default AboutPage;
--- app/project/page.tsx
+++ app/project/page.tsx
@@ -1,4 +1,32 @@
+export const generateMetadata = async ({
+	params,
+}: {
+	params: { slug: string };
+}) => {
+	return {
+		title: 'Todos mis Proyectos | Martin Ferreira',
+		authors: [
+			{
+				name: 'Martin Ferreira Yic',
+				url: 'https://www.linkedin.com/in/martin-ferreira-yic/',
+			},
+		],
+		icons: [
+			{
+				rel: 'icon',
+				url: 'data:image/svg+xml,<svg xmlns=%22http://www.w3.org/2000/svg%22 viewBox=%220 0 100 100%22><text y=%22.9em%22 font-size=%2290%22>👨🏻‍💻</text></svg>',
+			},
+		],
+	};
+};
+
 const ProjectPage = () => {
-	return <h1>ProjectPage</h1>;
+	return (
+		<section className="h-screen">
+			<h1 className="text-center text-4xl md:text-6xl mb-11">Proyectos</h1>
+			<p className="text-8xl text-center  mb-11">🏗️</p>
+			<p className="text-2xl text-center">👷🏻 En construcción ⚒️</p>
+		</section>
+	);
 };
 export default ProjectPage;
