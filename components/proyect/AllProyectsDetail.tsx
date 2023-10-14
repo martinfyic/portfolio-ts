1d1392b975872c707ce314c4a6181f46ded90daa
Martin Ferreira <martin.f.yic@gmail.com>
2023-10-14 13:01:36 -0300

    feat: :sparkles: add all proyect

---
+++ components/proyect/AllProyectsDetail.tsx
@@ -0,0 +1,41 @@
+import { FC } from 'react';
+import Image from 'next/image';
+import NextLink from 'next/link';
+
+import { BsArrowUpRightSquare } from 'react-icons/bs';
+
+import { IProyect } from '@/interfaces';
+
+interface AllProyectsDetailProps {
+	proyect: IProyect;
+}
+
+export const AllProyectsDetail: FC<AllProyectsDetailProps> = ({ proyect }) => {
+	const { image, name, slug } = proyect;
+	return (
+		<div className="p-4">
+			<Image
+				src={image}
+				alt={name}
+				width={400}
+				height={400}
+				className="rounded shadow-lg"
+				priority
+				data-aos="zoom-in"
+			/>
+
+			<h2 className="text-sm text-center font-semibold md:text-lg p-2 my-3">
+				{name}
+				<hr className="w-20 h-1 mx-auto my-4 bg-primary border-0 rounded" />
+			</h2>
+			<div className="flex justify-center items-center mt-3">
+				<NextLink href={`/project/${slug}`}>
+					<BsArrowUpRightSquare
+						size={30}
+						className="hover:-translate-y-1 transition-transform cursor-pointer"
+					/>
+				</NextLink>
+			</div>
+		</div>
+	);
+};
