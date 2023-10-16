134565d2bf2b23fe5df530c74da595246f08638d
Martin Ferreira <martin.f.yic@gmail.com>
2023-10-15 23:19:13 -0300

    feat: :sparkles: implement Arrow

--- components/hero/HeroSection.tsx
+++ components/hero/HeroSection.tsx
@@ -1,15 +1,18 @@
 'use client';
 import { FC } from 'react';
+
 import { HeroImage } from './HeroImage';
 import { HeroDetail } from './HeroDetail';
+import { ArrowLink } from '../ui';
 
 export const HeroSection: FC = () => {
 	return (
 		<section id="home">
-			<div className="flex flex-col text-center items-center justify-center my-10 py-16 md:flex-row md:py-24 md:my-20 lg:my-36">
+			<div className="flex flex-col text-center items-center justify-center md:flex-row my-10 py-10 xl:my-24 xl:py-24">
 				<HeroImage />
 				<HeroDetail />
 			</div>
+			<ArrowLink />
 		</section>
 	);
 };
