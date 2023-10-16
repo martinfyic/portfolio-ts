467bbcf49ccf1fb28361337974732986baca0b9a
Martin Ferreira <martin.f.yic@gmail.com>
2023-10-15 23:18:49 -0300

    feat: :sparkles: add components/ui/ArrowLink

---
+++ components/ui/ArrowLink.tsx
@@ -0,0 +1,9 @@
+import { HiMiniChevronDoubleDown } from 'react-icons/hi2';
+
+export const ArrowLink = () => {
+	return (
+		<div className="flex flex-row items-center text-center justify-center mb-32">
+			<HiMiniChevronDoubleDown size={35} className="animate-bounce" />
+		</div>
+	);
+};
