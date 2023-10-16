0a29a0d831f0c0eb3bfd413d82ae3fb0ca7743bd
Martin Ferreira <martin.f.yic@gmail.com>
2023-10-16 17:56:11 -0300

    refactor: :recycle: refactor component

--- components/ui/ArrowLink.tsx
+++ components/ui/ArrowLink.tsx
@@ -1,9 +1,18 @@
+import { FC } from 'react';
+import NextLink from 'next/link';
+
 import { HiMiniChevronDoubleDown } from 'react-icons/hi2';
 
-export const ArrowLink = () => {
+interface Props {
+	to: string;
+}
+
+export const ArrowLink: FC<Props> = ({ to }) => {
 	return (
 		<div className="flex flex-row items-center text-center justify-center mb-32">
-			<HiMiniChevronDoubleDown size={35} className="animate-bounce" />
+			<NextLink href={to}>
+				<HiMiniChevronDoubleDown size={35} className="animate-bounce" />
+			</NextLink>
 		</div>
 	);
 };
