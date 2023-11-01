4d4aeddb242c41e5c3cc3944438ddcde8b326305
Martin Ferreira <martin.f.yic@gmail.com>
2023-11-01 19:04:05 -0300

    refactor: :lipstick: add backgraund pattern

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,33 +1,34 @@
-'use client';
+"use client";
 
-import { ReactNode, useEffect } from 'react';
+import { ReactNode, useEffect } from "react";
 
-import AOS from 'aos';
-import 'aos/dist/aos.css';
+import AOS from "aos";
+import "aos/dist/aos.css";
 
-import { Footer } from '@/components';
-import { Providers } from './providers';
-import 'animate.css';
-import '../styles/globals.css';
-import 'react-toastify/dist/ReactToastify.css';
+import { Footer } from "@/components";
+import { Providers } from "./providers";
+import "animate.css";
+import "../styles/globals.css";
+import "react-toastify/dist/ReactToastify.css";
 
-import { Inter } from 'next/font/google';
+import { Inter } from "next/font/google";
 
-const inter = Inter({ subsets: ['latin'] });
+const inter = Inter({ subsets: ["latin"] });
 
 export default function RootLayout({ children }: { children: ReactNode }) {
-	useEffect(() => {
-		AOS.init();
-	}, []);
+  useEffect(() => {
+    AOS.init();
+  }, []);
 
-	return (
-		<html lang="es" suppressHydrationWarning className="scroll-smooth">
-			<body className={inter.className}>
-				<Providers>
-					{children}
-					<Footer />
-				</Providers>
-			</body>
-		</html>
-	);
+  return (
+    <html lang="es" suppressHydrationWarning className="scroll-smooth">
+      <body className={inter.className}>
+        <div className="absolute top-0 z-[-2] h-screen w-screen bg-white bg-[radial-gradient(ellipse_80%_80%_at_50%_-20%,rgba(120,119,198,0.3),rgba(255,255,255,0))] dark:bg-darker dark:bg-[radial-gradient(ellipse_80%_80%_at_50%_-20%,rgba(120,119,198,0.3),rgba(255,255,255,0))]"></div>
+        <Providers>
+          {children}
+          <Footer />
+        </Providers>
+      </body>
+    </html>
+  );
 }
