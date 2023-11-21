5b4eb9bb22d1958a93d50891e537c4f5be43b5a4
Martin Ferreira <martin.f.yic@gmail.com>
2023-11-21 20:16:32 -0300

    feat: :art: implement SubTitleSection

--- components/about/AboutSection.tsx
+++ components/about/AboutSection.tsx
@@ -1,13 +1,11 @@
+import { SubTitleSection } from "../ui";
 import { BiografiSection, SkillSection } from "./";
 
 export const AboutSection = () => {
   return (
     <section id="about" className="py-12">
       <div className="my-12 pb-12 md:pb-24 md:pt-6">
-        <h2 className="mb-12 mt-6 text-center text-4xl font-bold md:text-6xl">
-          Sobre Mi
-          <hr className="mx-auto my-4 h-1 w-6 rounded border-0 bg-primary" />
-        </h2>
+        <SubTitleSection text="Sobre Mi" />
         <div className="flex flex-col items-stretch justify-center space-y-10 align-top md:flex-row md:space-x-10 md:space-y-0 md:p-4 md:text-left">
           <BiografiSection />
           <SkillSection />
