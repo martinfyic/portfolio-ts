475f6c7bfed4a7cf9d34ac32957a1437166e75d0
Martin Ferreira <martin.f.yic@gmail.com>
2024-03-14 19:49:53 -0300

    chore: :lipstick: cambio de posicion HeroImage por HeroDetail

--- components/hero/HeroSection.tsx
+++ components/hero/HeroSection.tsx
@@ -9,8 +9,8 @@ export const HeroSection: FC = () => {
   return (
     <section id="home" className="relative">
       <div className="my-10 flex flex-col items-center justify-center py-10 text-center lg:flex-row xl:my-24 xl:py-24">
-        <HeroImage />
         <HeroDetail />
+        <HeroImage />
       </div>
       <ArrowLink to="#about" />
     </section>
