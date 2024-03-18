50b5cbf32f9e6f9cc492956a34ff5a9b2e624824
Martin Ferreira <martin.f.yic@gmail.com>
2024-03-18 20:01:34 -0300

    chore: :art: actualizo archivo de exportacion

--- helpers/index.ts
+++ helpers/index.ts
@@ -1,5 +1,5 @@
 export * from "./getCertificateInfo";
-export * from "./getProyectInfo";
+export * from "./getProjectInfo";
 export * from "./getPaths";
 export * from "./index";
 export * from "./nodemailer";
