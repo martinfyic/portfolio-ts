c756e9530e5c91f15132a3756bb2dfa9a533862d
Martin Ferreira <martin.f.yic@gmail.com>
2024-03-18 20:01:11 -0300

    fix: :truck: renombro archivo

--- helpers/getProjectInfo.ts
+++ helpers/getProjectInfo.ts
@@ -1,7 +1,7 @@
 import { projects } from "@/data";
 import { IProject } from "@/interfaces";
 
-export const getProyectInfo = async (slug: string): Promise<IProject> => {
+export const getProjectInfo = async (slug: string): Promise<IProject> => {
   const data = projects.filter((p) => p.slug === slug);
   return data[0];
 };
