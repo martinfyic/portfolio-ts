2036380a0070ade599e3346ce406abcac4376494
martinfyic <martin.f.yic@gmail.com>
2024-05-07 22:01:20 -0300

    refactor: :lipstick: remove profile image from hero

--- components/hero/HeroSection.tsx
+++ components/hero/HeroSection.tsx
@@ -1,16 +1,14 @@
 "use client";
 import { FC } from "react";
 
-import { HeroImage } from "./HeroImage";
 import { HeroDetail } from "./HeroDetail";
 import { ArrowLink } from "../ui";
 
 export const HeroSection: FC = () => {
   return (
     <section id="home" className="relative">
-      <div className="my-10 flex flex-col items-center justify-center py-10 text-center lg:flex-row xl:my-24 xl:py-24">
+      <div className="my-24 flex flex-col items-center justify-center py-10 text-center md:my-28 lg:flex-row xl:my-24 xl:py-24">
         <HeroDetail />
-        <HeroImage />
       </div>
       <ArrowLink to="#about" />
     </section>
